a240d825117fe996e9bf3d81bb90c10ecadf7c83
Yevhenii <zhenya.sil96@gmail.com>
2022-01-21 18:12:33 +0200

    Route Music, Route News and Route Settings

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import Dialogs from './components/Dialogs/Dialogs';
+import News from './components/News/News';
+import Music from './components/Music/Music';
+import Settings from './components/Settings/Settings';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -15,8 +18,11 @@ function App() {
         <Navbar />
         <div className='app-wrapper-content'>
           <Routes>
-            <Route path = '/dialogs' element={<Dialogs/>} />
-            <Route path = '/profile' element={<Profile/>} />
+            <Route path='/dialogs' element={<Dialogs />} />
+            <Route path='/profile' element={<Profile />} />
+            <Route path='/news' element={<News />} />
+            <Route path='/music' element={<Music />} />
+            <Route path='/settings' element={<Settings />} />
           </Routes>
         </div>
       </div>
